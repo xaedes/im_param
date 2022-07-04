04fa98a6db73543035cce1c5315a2205458a3572
xaedes <xaedes@gmail.com>
2022-07-04 19:25:55 +0200

    add c++ 11 implementation of c++ 14 feature: conditional_t

--- im_param/include/im_param/detail/cpp11_template_logic.h
+++ im_param/include/im_param/detail/cpp11_template_logic.h
@@ -4,20 +4,25 @@
 
 namespace im_param {
 
+    #pragma region c++11 implementations of c++14 template functions
+    template<bool C, class A, class B>
+    using conditional_t = typename std::conditional<C,A,B>::type;
+    #pragma endregion
+
     #pragma region c++11 implementations of c++17 template logic functions
     // https://en.cppreference.com/w/cpp/types/conjunction
     template<class...> struct conjunction : std::true_type { };
     template<class B1> struct conjunction<B1> : B1 { };
     template<class B1, class... Bn>
     struct conjunction<B1, Bn...> 
-        : std::conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
+        : conditional_t<bool(B1::value), conjunction<Bn...>, B1> {};
 
     // https://en.cppreference.com/w/cpp/types/disjunction
     template<class...> struct disjunction : std::false_type { };
     template<class B1> struct disjunction<B1> : B1 { };
     template<class B1, class... Bn>
     struct disjunction<B1, Bn...> 
-        : std::conditional_t<bool(B1::value), B1, disjunction<Bn...>>  { };
+        : conditional_t<bool(B1::value), B1, disjunction<Bn...>>  { };
 
     // https://en.cppreference.com/w/cpp/types/negation
     template<class B>
