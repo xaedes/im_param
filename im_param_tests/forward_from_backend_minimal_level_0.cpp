5b46cc73586f52174f59e01f1b62372b13f6e452
xaedes <xaedes@gmail.com>
2022-07-05 13:47:21 +0200

    temporarily add test forward_from_backend_minimal_level_0a_wont_compile_on_gcc to see if it really fails to compile (it does compile with visual studio tho)

--- im_param_tests/forward_from_backend_minimal_level_0.cpp
+++ im_param_tests/forward_from_backend_minimal_level_0.cpp
@@ -41,30 +41,4 @@ int forward_from_backend_minimal_level_0(int argc, char* argv[]) {
     return 0;
 }
 
-//  Adding another template class won't work resulting in "undefined reference to `void forwarded<float, int>(float, int)'" error or similar on gcc:
-//  
-//  #include <iostream>
-//  #include <type_traits>
-//  
-//  template<class U, class T>
-//  void forwarded(U user, T val);
-//  
-//  template<class U, class T>
-//  void backend(U user, T val)
-//  {
-//      std::cout << "backend" << "\n";
-//      forwarded(user, val);
-//  }
-//  
-//  template<class U>
-//  void forwarded(U user, int val)
-//  {
-//      std::cout << "forwarded" << user << "\n";
-//  }
-//  
-//  int forward_from_backend_minimal_level_0() {
-//      float u;
-//      int i=1;
-//      backend(u, i);
-//  }
-//  
+
