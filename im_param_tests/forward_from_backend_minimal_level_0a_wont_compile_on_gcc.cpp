e01e4a5d7c4353bbf149c05d01b4f414cfaf2cb5
xaedes <xaedes@gmail.com>
2022-07-05 13:56:53 +0200

    add build-error log from ci and disable the tests that is expected to not compile

--- im_param_tests/forward_from_backend_minimal_level_0a_wont_compile_on_gcc.cpp
+++ im_param_tests/forward_from_backend_minimal_level_0a_wont_compile_on_gcc.cpp
@@ -1,7 +1,14 @@
 
-//  Adding another template class won't work resulting in 
-//  "undefined reference to `void forwarded<float, int>(float, int)'" error 
-//  or similar on gcc:
+// Adding another template class won't work resulting in linkage errors on some platforms.
+// It does work for windows with Visual Studio.
+// 
+// Linkage error on ubuntu:focal and other gcc:
+// 
+//  > /usr/bin/ld: im_param_tests/CMakeFiles/im_param_tests.dir/forward_from_backend_minimal_level_0a_wont_compile_on_gcc.cpp.o: in function `forward_from_backend_minimal_level_0a_wont_compile_on_gcc(int, char**)':
+//  > forward_from_backend_minimal_level_0a_wont_compile_on_gcc.cpp:(.text+0x42): undefined reference to `void forwarded<float, int>(float, int)'
+//  > collect2: error: ld returned 1 exit status
+//
+
 
 #include <iostream>
 #include <type_traits>
